9ba86a095821dd5898371905c8649a03281b4511
Srilochan <srilochan7@gmail.com>
2025-01-21 13:14:32 +0530

    props-basic

--- tailwind-props/src/App.jsx
+++ tailwind-props/src/App.jsx
@@ -1,21 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import './index.css'; // or wherever your tailwind styles are located
+import { useState } from 'react';
 
+import './App.css';
+import './index.css'; // or wherever your Tailwind styles are located
+
+import Card from './components/card.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <div className="bg-green-500 p-4 text-white">
-  Tailwind is working!
-</div>
+      <div className="bg-green-500 p-4 text-white">
+        Tailwind is working!
+      </div>
+      <br></br>
+      <br></br>
+      
+      <Card sub="Monkey NFT 1" price="$100" rate="0.11"/>
+
+      <Card sub="Monkey NFT 2" price="$99" rate="0.10"   />
 
     </>
-  )
+  );
 }
 
-export default App
+export default App;
